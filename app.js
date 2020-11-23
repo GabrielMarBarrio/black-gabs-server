0f49111d4350a2648afff3f69331fbd2e1218cf5
Luis Fernando Félix Mata <51014165+LuisFerUACH@users.noreply.github.com>
2020-11-22 21:57:55 -0700

    Update app.js

---
+++ app.js
@@ -0,0 +1,29 @@
+const http = require('http');
+const fs = require('fs');
+
+http.createServer((request, response) => {
+
+    if(request.url){
+
+        const file = request.url == '/' ? './WWW/index.html' : ./WWW${request.url};
+
+        fs.readFile(file, (err, data) => {
+
+            if(err){
+                response.writeHead(404, {"Content-Type": "text/html"});
+                response.write("NOT FOUND");
+            }else{
+                if(file.split('.').pop() == 'json'){
+                    response.writeHead(200, {"Content-Type": "application/json"});
+                }else if(file.split('.').pop() == 'pdf'){
+                    response.writeHead(200, {"Content-Type": "application/pdf"}); 
+                }else{
+                    response.writeHead(200, {"Content-Type": "text/html"}); 
+                }
+                response.write(data);
+            }
+            response.end();
+        });
+    }
+
+}).listen(4000);
